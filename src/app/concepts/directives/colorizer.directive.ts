0165fa8fb7bce9ec2923d9a261f4db564337868c
Arun Vijayarengan <arunprabu.v@hotmail.com>
2021-01-21 11:35:23 +0530

    updated

--- src/app/concepts/directives/colorizer.directive.ts
+++ src/app/concepts/directives/colorizer.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, ElementRef } from '@angular/core';
+import { Directive, ElementRef, HostListener, Renderer2 } from '@angular/core';
 
 // Decorator
 @Directive({
@@ -6,15 +6,31 @@ import { Directive, ElementRef } from '@angular/core';
 })
 export class ColorizerDirective {
 
-  constructor( private elRef: ElementRef ) {
+  constructor( private elRef: ElementRef, private renderer: Renderer2) { // dependency injection
     console.log('applying directive');
     console.log(this.elRef.nativeElement);
 
-    // changing dom with JS
     let el = this.elRef.nativeElement;
-    el.style.backgroundColor = 'red';
-    el.style.color = '#fff';
 
+    // changing dom with JS
+    // el.style.backgroundColor = 'red';
+    // el.style.color = '#fff';
+
+    this.renderer.setStyle(el, 'background-color', 'red');
+    this.renderer.setStyle(el, 'color', '#fff');
+    this.renderer.setStyle(el, 'height', '400px');
+  }
+
+  // @HostListener() is a decorator
+  @HostListener('click', ['$event'])
+  onClickHandler(evt): void{
+    this.renderer.setStyle(evt.target, 'background-color', 'green');
+  }
+
+  @HostListener('mouseover', ['$event'])
+  onMouseover(evt): void {
+    this.renderer.setStyle(evt.target, 'background-color', 'yellow');
+    this.renderer.setStyle(evt.target, 'color', 'black');
   }
 
 }
