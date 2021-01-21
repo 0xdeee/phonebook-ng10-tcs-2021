7af714752b5a729bbde712ecbf32a900731dbda4
Arun Vijayarengan <arunprabu.v@hotmail.com>
2021-01-21 13:22:22 +0530

    updated

--- src/app/contacts/components/add-contact/add-contact.component.ts
+++ src/app/contacts/components/add-contact/add-contact.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-add-contact',
@@ -8,9 +9,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddContactComponent implements OnInit {
 
+  // Step 1: Have form tag equivalent in TS
+  addContactForm: FormGroup;
+
   constructor() { }
 
   ngOnInit(): void {
+    // Step 1 continues...
+    this.addContactForm = new FormGroup({
+      // Step 2: Have Form control equivalents in TS
+      name: new FormControl('', Validators.required), // Step 5: work on validation
+      phone: new FormControl('', Validators.required),
+      email: new FormControl('', [ Validators.required, Validators.email ])
+    });
+
+    // Refer html for step 3 of reactive forms
+  }
+
+  addContactHandler(): void {
+    console.log(this.addContactForm); // you can find the whole form's state
+    console.log(this.addContactForm.value);
   }
 
 }
